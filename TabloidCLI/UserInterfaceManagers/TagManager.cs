d97ebdcb2760ce6fed9df5974dece8e898a9997a
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-09-11 09:48:04 -0500

    Added edit and remove for tag

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -60,6 +60,37 @@ namespace TabloidCLI.UserInterfaceManagers
             }
         }
 
+        private Tag Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Tag:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.WriteLine("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return tags[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Seletion");
+                return null;
+            }
+        }
+
         private void Add()
         {
             Console.WriteLine("New Tag");
@@ -73,12 +104,30 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void Edit()
         {
-           // Tag tagToEdit = Choose
+            Tag tagToEdit = Choose("Which tag would you like to edit?");
+            if (tagToEdit == null)
+            {
+                return;
+            }
+
+            Console.WriteLine();
+            Console.Write("New name (blank to leave unchanged): ");
+            string name = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                tagToEdit.Name = name;
+            }
+
+            _tagRepository.Update(tagToEdit);
         }
 
         private void Remove()
         {
-            throw new NotImplementedException();
+            Tag tagToDelete = Choose("Which tag would you like to remove?");
+            if (tagToDelete != null)
+            {
+                _tagRepository.Delete(tagToDelete.Id);
+            }
         }
     }
 }
